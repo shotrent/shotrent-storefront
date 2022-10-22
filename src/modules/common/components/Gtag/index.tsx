7203c7be93ac289344ac2ed9954c99d4a3644a5a
kudchikarsk <kudchikarsk@gmail.com>
2022-10-22 13:59:27 +0530

    update gtag

---
+++ src/modules/common/components/Gtag/index.tsx
@@ -0,0 +1,28 @@
+import Script from "next/script"
+import React from "react"
+
+
+const Gtag: React.FC = () => {
+  const isProduction = true;//process.env.NODE_ENV === "production";
+
+  return (
+    <>
+      {isProduction && (
+        <>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-957359900"></Script>
+          <Script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-957359900');
+          `}
+          </Script>
+        </>)}
+    </>
+  )
+}
+
+export default Gtag;
--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -1,5 +1,7 @@
 import NextHead from "next/head"
+import Script from "next/script"
 import React from "react"
+import Gtag from "../Gtag"
 
 type HeadProps = {
   title?: string
@@ -8,32 +10,19 @@ type HeadProps = {
 }
 
 const Head: React.FC<HeadProps> = ({ title, description, image }) => {
-  const isProduction = process.env.NODE_ENV === "production";
+  const isProduction = true;//process.env.NODE_ENV === "production";
 
   return (
-    <NextHead>
-      <title>{title} | Shotrent</title>
-      <meta itemProp="name" content={title} />
-      {description && <meta itemProp="description" content={description} />}
-      {image && <meta itemProp="image" content={image} />}
-      <link rel="icon" href="/favicon.ico" />
-
-      {isProduction ?(
-      <>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-957359900"></script>
-        <script>
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'AW-957359900');
-          `}
-        </script>
-      </>): ""}
-
-    </NextHead>
+    <>
+      <NextHead>
+        <title>{title} | Shotrent</title>
+        <meta itemProp="name" content={title} />
+        {description && <meta itemProp="description" content={description} />}
+        {image && <meta itemProp="image" content={image} />}
+        <link rel="icon" href="/favicon.ico" />
+      </NextHead>
+      <Gtag />
+    </>
   )
 }
 
