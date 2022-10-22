0b20b9fb553a8c934560249950275b0d349d5554
kudchikarsk <kudchikarsk@gmail.com>
2022-10-22 13:47:03 +0530

    move gtag to useeffect

--- src/pages/order/confirmed/[id].tsx
+++ src/pages/order/confirmed/[id].tsx
@@ -6,7 +6,7 @@ import OrderCompletedTemplate from "@modules/order/templates/order-completed-tem
 import SkeletonOrderConfirmed from "@modules/skeletons/templates/skeleton-order-confirmed"
 import { GetStaticPaths, GetStaticProps } from "next"
 import { useRouter } from "next/router"
-import { ReactElement } from "react"
+import { ReactElement, useEffect } from "react"
 import { dehydrate, QueryClient, useQuery } from "react-query"
 import { NextPageWithLayout } from "types/global"
 
@@ -28,6 +28,20 @@ const Confirmed: NextPageWithLayout = () => {
     }
   )
 
+  useEffect(()=>{
+    if(isSuccess) {
+      const gtag =  (window as  any).gtag;
+      if(gtag) {
+        gtag('event', 'conversion', {
+          'send_to': 'AW-957359900/z2t_CKCCl4AYEJzOwMgD',
+          'value': data.total,
+          'currency': data.currency_code,
+          'transaction_id': data.id,
+        });
+      }
+    }
+  }, [isSuccess, data]);
+
   if (isLoading) {
     return <SkeletonOrderConfirmed />
   }
@@ -41,16 +55,6 @@ const Confirmed: NextPageWithLayout = () => {
   }
 
   if (isSuccess) {
-    const gtag =  (window as  any).gtag;
-    if(gtag) {
-      gtag('event', 'conversion', {
-        'send_to': 'AW-957359900/z2t_CKCCl4AYEJzOwMgD',
-        'value': data.total,
-        'currency': data.currency_code,
-        'transaction_id': data.id,
-      });
-    }
-
 
     return (
       <>
