2118a0fdedc5d1fb83515abacf6a6d22bf8d7d68
kudchikarsk <kudchikarsk@gmail.com>
2023-02-20 01:26:50 +0530

    integrate kyc process

--- src/modules/order/templates/order-completed-template.tsx
+++ src/modules/order/templates/order-completed-template.tsx
@@ -15,44 +15,31 @@ type OrderCompletedTemplateProps = {
   order: Order
 }
 
+type DigioInstance = { init: () => void; submit: (arg0: any, arg1: any, arg2: any) => void } | undefined;
+
 const OrderCompletedTemplate: React.FC<OrderCompletedTemplateProps> = ({
   order,
 }) => {
 
   const [loading, setLoading] = useState(false);
-  let digio: { init: () => void; submit: (arg0: any, arg1: any, arg2: any) => void };
-  console.log(order)
-  const [isDocumentSigned, setIsDocumentSigned] = useState(false);
+  const [isKycRequestLoading, setIsKycRequestLoading] = useState(false);
+  const [orderMetadata, setorderMetadata] = useState({} as any);
 
-  const checkRentalAgreementStatus = () => {
+  const getOrderMetadata = () => {
     customClient.get(`/store/orders/${order.id}/metadata`)
-    .then(res=> setIsDocumentSigned(res.data.isRentalAgreementSigned));
+    .then(res=> setorderMetadata(res.data));
   }
 
   useEffect(()=>{
-    checkRentalAgreementStatus();
+    getOrderMetadata();
   }, [])
 
-  const initDigio = () => {
+  const initDigio = (callback:(arg1:any)=>void) => {
     const theWindow = window as any;
     if(theWindow) {
       const options = {
         environment : process.env.NODE_ENV === 'production'? 'production': 'sandbox',
-        callback : (response:any) => {
-          if(response.hasOwnProperty("error_code"))
-          {
-            return console.log("error occurred in process");
-          }          
-          console.log(response);
-          customClient.post(`/store/orders/${order.id}/digio/sign/agreement/response`)
-          .then(res=>{
-            console.log(res);
-            checkRentalAgreementStatus();
-          })
-          .catch(err=>{
-            console.log(err);
-          })
-        },
+        callback,
         logo : `${theWindow.location.origin}/favicon.ico`, 
         theme : {
           primaryColor : "#AB3498",
@@ -60,12 +47,13 @@ const OrderCompletedTemplate: React.FC<OrderCompletedTemplateProps> = ({
           },
         is_iframe: false
       }      
-      digio = new theWindow.Digio(options);
-      digio.init();
+      const digio:DigioInstance = new theWindow.Digio(options);
+      digio?.init();
+      return digio;
     }
   }
 
-  const submitDigio = (data: { requestId: any; identifier: any; token_id: any }) => {
+  const submitDigio = (digio:DigioInstance,data: { requestId: any; identifier: any; token_id: any }) => {
      if(digio) {
       digio.submit(data.requestId, data.identifier, data.token_id)
      }
@@ -76,27 +64,106 @@ const OrderCompletedTemplate: React.FC<OrderCompletedTemplateProps> = ({
   const createSignRequest = () => {
     if(loading) return;    
     setLoading(true);    
-    customClient.post(`/store/orders/${order.id}/digio/sign/agreement`)
+    const digio = initDigio((response:any) => {
+      if(response.hasOwnProperty("error_code"))
+      {
+        return console.log("error occurred in process");
+      }          
+      customClient.post(`/store/orders/${order.id}/digio/sign/agreement/response`)
       .then(res=>{
-        if(!digio) {
-          initDigio();
-        }
-        console.log(res.data);
+        getOrderMetadata();
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    });
+
+    customClient.post(`/store/orders/${order.id}/digio/sign/agreement`)
+      .then(res=>{        
         const result = res.data;
-        const params = {requestId: result.data.id, identifier: result.data.signing_parties[0].identifier, token_id: result.data.access_token.id};
-        console.log(params);
-        submitDigio(params);
+        const params = {
+          requestId: result.data.id, 
+          identifier: result.data.signing_parties[0].identifier, 
+          token_id: result.data.access_token.id
+        };
+        submitDigio(digio, params);
       })
       .finally(()=> setLoading(false));
   }
 
+  const createKycRequest = () => {
+    if(isKycRequestLoading) return; 
+    setIsKycRequestLoading(true);
 
+    const digio = initDigio((response:any) => {
+      if(response.hasOwnProperty("error_code"))
+      {
+        return console.log("error occurred in process");
+      }          
+      customClient.post(`/store/orders/${order.id}/digio/kyc/response `)
+      .then(res=>{
+        getOrderMetadata();
+      })
+      .catch(err=>{
+        console.log(err);
+      })       
+    });
+
+    customClient.post(`/store/orders/${order.id}/digio/kyc`)
+    .then(response=>{      
+      const result = response.data;
+      const params = {
+        requestId: result.data.id, 
+        identifier: result.data.customer_identifier, 
+        token_id: result.data.access_token.id
+      };
+      submitDigio(digio, params);
+    })
+    .catch(err=> console.log(err))
+    .finally(()=> setIsKycRequestLoading(false))
+  }
+
+  type ShowStatusProps = {isCompleted:boolean};
+  const ShowStatus = (props:ShowStatusProps) => {
+    return (
+      props.isCompleted?(<span className="rounded-full text-white bg-green-500 py-1 px-2 text-xs">completed</span>)
+      :(<span className="rounded-full text-white bg-blue-500 py-1 px-2 text-xs">pending</span>)
+    )
+  }
 
   return (
     <div className="bg-gray-50 py-6 min-h-[calc(100vh-64px)]">
       <div className="content-container flex justify-center">
         <div className="max-w-4xl h-full bg-white w-full">
           <OrderDetails order={order} />
+
+          <div className="p-10 border-b border-gray-200">
+            <h1 className="text-base-regular mb-0">Please complete below steps to confirm your order</h1>
+            <div className="mt-8">
+              <div className="flex justify-between">
+                <h2 className="text-base-semi">Step 1: E-sign agreement</h2>
+                <ShowStatus isCompleted={orderMetadata.isRentalAgreementSigned} />
+              </div>
+              <div className="mt-2">
+                <span className="text-small-regular text-gray-700">You will be redirected to Digio to complete this step. The agreement will be digitally signed by your Aadhaar.</span>
+                {!orderMetadata.isRentalAgreementSigned?
+                (<Button className="w-64 mt-4" onClick={e=>createSignRequest()}>{loading?'Loading...':'e-sign rental agreement'}</Button>)
+                :(<Button disabled className="w-64 mt-4">{'e-sign completed'}</Button>)}
+              </div>              
+            </div>
+            <div className="mt-12">
+              <div className="flex justify-between">
+                <h2 className="text-base-semi">Step 2: KYC</h2> 
+                <ShowStatus isCompleted={orderMetadata.isKycCompleted} />
+              </div>
+              <div className="mt-2">
+                <span className="text-small-regular text-gray-700">You will be redirected to DigiLocker to complete this step. Your KYC documents <span className="font-semibold">Aadhaar and PAN</span> will be fetched from DigiLocker.</span>
+                {!orderMetadata.isKycCompleted?(<Button className="w-64 mt-4" onClick={e=>createKycRequest()}>{isKycRequestLoading?'Loading...':'complete kyc'}</Button>)
+                :(<Button disabled className="w-64 mt-4">{'kyc completed'}</Button>)}
+              </div>
+            </div>
+          </div>
+
           <Items
             items={order.items}
             region={order.region}
@@ -112,18 +179,7 @@ const OrderCompletedTemplate: React.FC<OrderCompletedTemplateProps> = ({
               address={order.shipping_address}
             />
           </div>
-          <div className="p-10 border-b border-gray-200">
-            <h1 className="text-base-regular">Complete below steps to confirm your order</h1>
-            <div className="mt-8">
-              <h2 className="text-base-semi">Step 1: Sign rental agreement</h2>
-              <span className="text-small-regular text-gray-700">You will be redirected to digio to completed this step. The agreement will be digitally signed by your Aadhaar.</span>
-              {!isDocumentSigned?
-              (<Button className="w-64 mt-4" onClick={e=>createSignRequest()}>{loading?'Loading...':'e-sign rental agreement'}</Button>)
-              :(<Button className="w-64 mt-4">{'e-sign completed'}</Button>)}
-              
-            </div>
-            
-          </div>
+          
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-10 order-b border-gray-200">
             <Help />
             <OrderSummary order={order} />
